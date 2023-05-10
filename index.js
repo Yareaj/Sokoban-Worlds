23f1700c6a58f8e57257e1d93a10b83b8522b27f
Yareaj <shaulyareaj@gmail.com>
2023-05-10 14:39:59 -0500

    Adeed automated limit and verifications for walls trespass

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-// Definition of global variables for scope
-let levelMap, images;
-const playerPos = { row: 0, col: 0 };
+// Definition of quadrilles for scope to reach all functions
+let levelMap, playerQuad;
+
+// Definition of asset objects
+let images;
+const playerPos = { row: 2, col: 2 };
 
 // Game board size
-const columns = 5, rows = 5;
+const columns = 10, rows = 10;
 
 // Redefine global variables
-//Quadrille.CELL_LENGTH = 30;
-//Quadrille.OUTLINE_WEIGHT = 0;
+Quadrille.CELL_LENGTH = 50;
+Quadrille.OUTLINE_WEIGHT = 0;
 
 // Load assets before the quadrille is rendered
 function preload() {
@@ -21,8 +24,8 @@ function preload() {
         player: {
             up: loadImage('./assets/player/facingUp.png'),
             down: loadImage('./assets/player/facingDown.png'),
-            right: loadImage('./assets/player/facingRight.png'),
-            left: loadImage('./assets/player/facingLeft.png')
+            left: loadImage('./assets/player/facingLeft.png'),
+            right: loadImage('./assets/player/facingRight.png')
         }
     }
 }
@@ -31,32 +34,83 @@ function setup() {
     // Adjust the canvas to the board's exact measurements
     createCanvas(Quadrille.CELL_LENGTH * columns, Quadrille.CELL_LENGTH * rows);
 
-    // Define the level's map
-    levelMap = createQuadrille([
-        [ 'a', 'b', 'c' ],
-        [ 'd', 'e', 'f' ],
-        [ 'g', 'h', 'i' ]
-    ]);
+    // Define the level's map dimensions
+    levelMap = createQuadrille(rows, columns);
+
+    // Creates an independent quadrille for the player
+    playerQuad = createQuadrille([ images.player.up ]);
+
+    // Make all first/last columns/rows to be walls
+    for (let itRows=0; itRows<rows; itRows++) {
+        for (let itColumns=0; itColumns<columns; itColumns++) {
+            if (itRows == 0 || itRows == rows-1 || itColumns == 0 || itColumns == columns-1) {
+                levelMap._memory2D[itRows][itColumns] = images.blocks.wall;
+            } else {
+                levelMap._memory2D[itRows][itColumns] = images.blocks.grayFloor;
+            }
+        }
+    }
 }
 
 function draw() {
     drawQuadrille(levelMap);
-    // This isn't a viable option, it won't clear prior edited cells
-    levelMap.fill(playerPos.row, playerPos.col, images.player.down);
+
+    // Insert the player onto the main quadrille given a specific position
+    drawQuadrille(playerQuad, {
+        x: playerPos.col * Quadrille.CELL_LENGTH,
+        y: playerPos.row * Quadrille.CELL_LENGTH,
+        outline: 'green'
+    });
 }
 
 // Actions based upon the key pressed
 function keyPressed() {
     if (keyCode === UP_ARROW || key === 'w') {
-        console.log('up');
-        playerPos.row += 1;
-        //playerPos.row += 1;
+        playerQuad._memory2D[0][0] = images.player.up;
+
+        if (!wallAhead(playerPos.row, playerPos.col, 'up')) {
+            playerPos.row -= 1;
+        }        
     } else if (keyCode === DOWN_ARROW || key === 's') {
-        console.log('down');
-        //playerPos.row -= 1;
-    } else if (keyCode === RIGHT_ARROW || key === 'd') {
-        console.log('right');
+        playerQuad._memory2D[0][0] = images.player.down;
+
+        if (!wallAhead(playerPos.row, playerPos.col, 'down')) {
+            playerPos.row += 1;
+        } 
     } else if (keyCode === LEFT_ARROW || key === 'a') {
-        console.log('left');
+        playerQuad._memory2D[0][0] = images.player.left;
+        
+        if (!wallAhead(playerPos.row, playerPos.col, 'left')) {
+            playerPos.col -= 1;
+        }
+    } else if (keyCode === RIGHT_ARROW || key === 'd') {
+        playerQuad._memory2D[0][0] = images.player.right;
+
+        if (!wallAhead(playerPos.row, playerPos.col, 'right')) {
+            playerPos.col += 1;
+        }
+    } 
+}
+
+// Function that given the player's position on the quadrille determines if next location is wall blocked
+function wallAhead(rowCord, colCord, direction) {
+    // Define the boolean to store wether or not the character will hit a wall
+    let isThereAWall = false;
+
+    // Object to define the final cell based on the direction
+    const dirInstructions = {
+        'up': { rowFinalCord: rowCord-1, colFinalCord: colCord },
+        'down': { rowFinalCord: rowCord+1, colFinalCord: colCord },
+        'left': { rowFinalCord: rowCord, colFinalCord: colCord-1 },
+        'right': { rowFinalCord: rowCord, colFinalCord: colCord+1 }
     }
+
+    // Define the coordinate the character will lead to
+    const destinyCell = [ dirInstructions[direction].rowFinalCord, dirInstructions[direction].colFinalCord ];
+    
+    if (levelMap.read(destinyCell[0], destinyCell[1]) == images.blocks.wall) {
+        isThereAWall = true
+    }
+
+    return isThereAWall;
 }
