7b0e6fb6895cf23ac977fbdd49685aa944be74e7
Yareaj <shaulyareaj@gmail.com>
2023-05-12 23:56:22 -0500

    Updated sources

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 /**
  * Some sources to include upon the credits
  * 
- * https://allbestfonts.com/
- * https://sfxr.me/
+ * https://allbestfonts.com/ (fonts)
+ * https://sfxr.me/ (forbidden + levelUp)
+ * https://freesound.org/people/EVRetro/sounds/535840/ (levelup)
+ * https://freesound.org/people/EVRetro/sounds/501102/ (footsteps)
  */
