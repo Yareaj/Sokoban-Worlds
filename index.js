40ec77bb662f6a1b056efd2cf798cf1f3cea508a
Yareaj <shaulyareaj@gmail.com>
2023-05-19 19:28:18 -0500

    First menu draft

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+let head;
 /**
  * Some sources to include upon the credits
  * 
@@ -7,11 +8,23 @@
  * https://freesound.org/people/EVRetro/sounds/501102/ (footsteps)
  */
 
-function setup() {
+function preload() {
+    head = loadImage('./assets/player/head.png');
+    loadFont('../sokoban/assets/fonts/SyntaxBlack.otf');
+}
 
+function setup() {
+    createCanvas(500, 500);
 }
 
 function draw() {
-    createCanvas(500, 500);
-    background('#6f6f')
+    background('#6f6f');
+    image(head, 0, 0);
+
+    textFont('SyntaxBlack');
+    textSize(40);
+    fill('#000');
+    const w = textWidth('Sokoban Worlds');
+    text(w, 100, 300);
+    text('Sokoban Worlds', 80, 100);
 }
